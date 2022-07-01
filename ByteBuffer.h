a13745a1f18176e96123fc3bd759835e9911fe4a
JNNGL <jnngles@gmail.com>
2022-07-01 22:30:17 +1100

    byte buffer

---
+++ ByteBuffer.h
@@ -0,0 +1,44 @@
+#include <vector>
+#include <string>
+
+class ByteBuffer {
+public:
+    ByteBuffer();
+    ByteBuffer(const unsigned char*, size_t);
+    
+public:
+    unsigned char readByte();
+    void writeByte(unsigned char);
+    
+    short readShort();
+    void writeShort(short);
+
+    int readInt();
+    void writeInt(int);
+
+    long long readLong();
+    void writeLong(long long);
+
+    std::string readString();
+    void writeString(std::string);
+
+    int readVarInt();
+    void writeVarInt(int);
+
+    long long readVarLong();
+    void writeVarLong(long long);
+
+    std::vector<unsigned char> readBytes(size_t);
+    void writeBytes(std::vector<unsigned char>);
+    void writeBytes(const unsigned char*, size_t);
+
+    void ensureWritableBytes(size_t);
+    size_t readableBytes();
+    size_t length();
+
+public:
+    size_t readerIdx = 0;
+    size_t writerIdx = 0;
+
+    std::vector<unsigned char> data;
+};
--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include "client.h"
+#include "ByteBuffer.h"
 #include <stdio.h>
 
 int main() {
+    ByteBuffer test;
+    test.writeByte(120);
+    test.writeInt(1234567);
+    test.writeLong(INT64_MAX);
+    test.writeVarLong(14235834664);
+    test.writeString("test string");
+
+    printf("%d\n", test.readByte());
+    printf("%d\n", test.readInt());
+    printf("%lld\n", test.readLong());
+    printf("%lld\n", test.readVarLong());
+    printf("%s\n", test.readString().data());
+    
     tcp_connect("localhost", 29077);
 }

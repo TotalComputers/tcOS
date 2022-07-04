39c90f62b8a7243c61dc6ee2173070f638ef8a08
JNNGL <jnngles@gmail.com>
2022-07-04 13:56:07 +1100

    Image slicing

---
+++ client/graphics/slicing.cpp
@@ -0,0 +1,20 @@
+#include "slicing.h"
+
+std::vector<unsigned char> slice_indices(std::vector<unsigned char> raw, int width, int height) {
+    std::vector<unsigned char> dst;
+    dst.resize(raw.size());
+
+    for(int y = 0; y < height / 128; y++) {
+        for(int x = 0; x < width / 128; x++) {
+            int idx = y * (width / 128) + x;
+            for(int sx = 0; sx < 128; sx++) {
+                for(int sy = 0; sy < 128; sy++) {
+                    dst[idx * 128 * 128 + sy * 128 + sx] =
+                        raw[(y * 128 + sy) * width + x * 128 + sx];
+                }
+            }
+        } 
+    }
+    
+    return dst;
+}
---
+++ client/graphics/slicing.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <vector>
+
+std::vector<unsigned char> slice_indices(std::vector<unsigned char>, int, int);

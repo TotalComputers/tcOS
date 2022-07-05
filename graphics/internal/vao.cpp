012572a84127ccae709146a83506c37fc146f0bc
JNNGL <jnngles@gmail.com>
2022-07-05 23:11:36 +1100

    Add missing attribute pointer functions

--- graphics/internal/vao.cpp
+++ graphics/internal/vao.cpp
@@ -17,9 +17,21 @@ void VAO::bind() {
     glBindVertexArray(handle);
 }
 
-void VAO::attribPointer3f(unsigned id, unsigned stride, long long offset) {
-    glVertexAttribPointer(id, 3, GL_FLOAT, GL_FALSE, stride * sizeof(float), (void*)offset);
+void VAO::attribPointerf(unsigned id, unsigned count, unsigned stride, long long offset) {
+    glVertexAttribPointer(id, count, GL_FLOAT, GL_FALSE, stride * sizeof(float), (void*)(offset * sizeof(float)));
     glEnableVertexAttribArray(id);
+}
+
+void VAO::attribPointer3f(unsigned id, unsigned stride, long long offset) {
+    attribPointerf(id, 3, stride, offset);
+}
+
+void VAO::attribPointer4f(unsigned id, unsigned stride, long long offset) {
+    attribPointerf(id, 4, stride, offset);
+}
+
+void VAO::attribPointer2f(unsigned id, unsigned stride, long long offset) {
+    attribPointerf(id, 2, stride, offset);
 }
 
 void VAO::unbind() {
--- graphics/internal/vao.h
+++ graphics/internal/vao.h
@@ -10,7 +10,10 @@ public:
 public:
     unsigned int getHandle();
     void bind();
+    void attribPointerf(unsigned, unsigned, unsigned, long long);
+    void attribPointer4f(unsigned, unsigned, long long);
     void attribPointer3f(unsigned, unsigned, long long);
+    void attribPointer2f(unsigned, unsigned, long long);
     static void unbind();
 
 private:

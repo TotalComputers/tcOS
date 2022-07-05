87f922b37fe03d42a6b6de77f18deccca3452201
JNNGL <jnngles@gmail.com>
2022-07-05 22:58:40 +1100

    fix thread safety

--- graphics/internal/glio.cpp
+++ graphics/internal/glio.cpp
@@ -1,14 +1,19 @@
 #include "glio.h"
 #include "../../common/thread_safety.h"
 #include <iostream>
+#include <mutex>
+
+std::mutex gl_mutex;
 
 GLIO::GLIO(IWindowFactory* factory)
     : factory(factory) {}
 
 void GLIO::init() {
+    gl_mutex.lock();
     run_in_main_thread([&]() {
         gl = factory->createWindow(reference_buf.width, reference_buf.height, name);
     });
+    gl_mutex.unlock();
     free(reference_buf.data);
     reference_buf.data = 0;
 }
@@ -18,18 +23,22 @@ void GLIO::set_frame(image_t frame)  {
 }
 
 void GLIO::destroy() {
+    gl_mutex.lock();
     run_in_main_thread([&]() {
         gl->destroy();
     });
     delete gl;
     gl = nullptr;
+    gl_mutex.unlock();
 }
 
 image_t& GLIO::provide_frame() {
     if(!gl) return reference_buf;
+    gl_mutex.lock();
     run_in_main_thread([&]() {
         gl->doLoopWork();
     });
+    gl_mutex.unlock();
     return ((PBOSurface*)gl->getSurface())->buffer;
 }
 
